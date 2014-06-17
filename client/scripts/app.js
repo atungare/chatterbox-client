65f99ff6d3477d34847d8bfe247a57822beae137
Hack Reactor Students <ajay.tungare@gmail.com>
2014-06-16 21:16:31 -0700

    Add buttons to increase functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,31 +2,55 @@
 
 var app = {};
 
-app.init = function(){
-  this.user = window.location.search.slice(10);
-  this.server = 'https://api.parse.com/1/classes/chatterbox';
+app.user = window.location.search.slice(10);
+app.server = 'https://api.parse.com/1/classes/chatterbox';
 
-  this.send({username:this.user,text:"hi",roomname:"lobby"});
+app.rooms = [];
+
+app.init = function(){
+
 
   $(".username").on('click', function(){
     app.addFriend();
   });
 
-  $("#send").on('click', function(){
+  $('#send').on('submit', function(e){
+    e.preventDefault();
+    e.stopPropagation();
     app.handleSubmit();
-    console.log("test");
   });
+
+  $(".refresh").on('click', function(){
+    app.clearMessages();
+    app.fetch();
+  });
+
+  $(".switchUser").on("click", function(){
+    var newName = prompt("Enter new user name:") || "anonymous";
+    if(app.isSafe(newName)){
+      app.user = newName;
+    }
+  });
+
+  $(".newRoom").on("click", function(){
+    app.addRoom();
+  });
+
+
+  app.fetch();
 };
 
 app.handleSubmit = function(){
   var myText = $("#message").val();
+
   $("#message").val("");
+
   var message = {
     username: this.user,
     text: myText,
-    roomname: 'lobby'
+    roomname: $("#room").val()
   };
-  console.dir(message);
+
   app.send(message);
 };
 
@@ -38,6 +62,7 @@ app.send = function(message){
     contentType: 'application/json',
     success: function(data) {
       console.log('chatterbox: Message sent', data);
+      $('.refresh').click();
     },
     error: function(data) {
       console.error('chatterbox: Failed to send message', data);
@@ -49,12 +74,24 @@ app.fetch = function(){
   $.ajax({
     url: app.server,
     type: 'GET',
+    data: {order: "-createdAt"},
     success: function(data){
-      for(var i=0; i<20; i++){
-        app.addMessage(data['results'][i]);
+      var counter = 0;
+      while( (counter < 100) && ($("#chats").children().length < 50) ){
+        app.addMessage(data['results'][counter]);
+        counter++;
+      }
+      app.clearRooms();
+      for(var i = 0; i < app.rooms.length; i++) {
+        var thisRoom = app.rooms[i];
+        $("#room").append("<option value=\""+thisRoom+"\">"+ thisRoom + "</option>");
       }
     }
   });
+};
+
+app.clearRooms = function() {
+  $("#room").empty();
 };
 
 app.clearMessages = function() {
@@ -62,16 +99,64 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(message) {
-  var msg = "<div class=\""+message.roomname+"\"><h2 class=\"username\">" + message.username + "</h2><p class=\"txt\">" + message.text + "</p></div>";
-  $("#chats").append(msg);
+  if(app.isSafe(message.roomname) && app.isSafe(message.text) && app.isSafe(message.username)){
+    if(app.rooms.indexOf(message.roomname) === -1) {
+      app.rooms.push(message.roomname);
+    }
+    var msg = "<div class=\""+message.roomname+"\"><h2 class=\"username\">" + message.username + "</h2><p class=\"txt\">" + message.text + "</p></div>";
+    $("#chats").append(msg);
+  }
 };
 
-app.addRoom = function(roomname){
-  $("#roomSelect").append("<div>"+roomname+"</div>");
+app.addRoom = function(){
+  var newRoom = prompt("Enter new room name:") || "lobby";
+  var newMessage = prompt("Enter your message:") || "Hello World!";
+  if(app.isSafe(newRoom) && app.isSafe(newMessage)){
+    var msg = {
+      username: app.user,
+      text: newMessage,
+      roomname: newRoom,
+    };
+    app.send(msg);
+  }
 };
 
 app.addFriend = function(){
 
 };
 
-app.init();
+app.isSafe = function(str){
+  var isSafe = true;
+  if(typeof str !== 'string'){
+    isSafe = false;
+  } else if (str.indexOf("<") > -1){
+    isSafe = false;
+  } else if (str.indexOf(">") > -1){
+    isSafe = false;
+  } else if (str.indexOf("\"") > -1){
+    isSafe = false;
+  } else if (str.indexOf("\/") > -1){
+    isSafe = false;
+  } else if (str.indexOf("\\") > -1){
+    isSafe = false;
+  } else if (str.indexOf("'") > -1){
+    isSafe = false;
+  } else if (str.indexOf("?") > -1){
+    isSafe = false;
+  } else if (str.indexOf("$") > -1){
+    isSafe = false;
+  } else if (str.indexOf("&") > -1){
+    isSafe = false;
+  } else if (str.indexOf("(") > -1){
+    isSafe = false;
+  } else if (str.indexOf(")") > -1){
+    isSafe = false;
+  } else if (str.indexOf("|") > -1){
+    isSafe = false;
+  }
+
+  return isSafe;
+};
+
+$(document).ready(function(){app.init();});
+
